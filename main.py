d04bab9c030e1c19c4d5a358feb03f12f9209252
Matthew Beaudouin-Lafon <matthew.beaudouin-lafon@students.olin.edu>
2019-05-17 20:49:59 -0400

    added user input with curses

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+import sys
+import select
+import time
+
+import curses
+from curses import textpad
+
 import receipt_printer
 from receipt_printer import Block_char, Player_char
 
@@ -9,15 +16,18 @@ class Player(object):
         self.direction = 0  # 0=Right, 1=Left
 
     def advance(self):
-        if self.direction == Player_char.RIGHT:
+        if self.direction == 0:  # Right
             self.position += 1
             if self.position == (receipt_printer.PAGE_WIDTH - 1):
                 self.change_direction()
 
-        elif self.direction == Player_char.LEFT:
+        elif self.direction == 1:  # Left
             self.position -= 1
             if self.position == 0:
                 self.change_direction()
+
+        else:
+            print("INVALID DIRECTION")
 
     def change_direction(self):
         self.direction = (self.direction + 1) % 2
@@ -36,32 +46,60 @@ class Player(object):
 #     def __init__(self, position=None):
 #         if position == None:
 
-def other_direction(direction):
-    if direction == Player_char.LEFT:
-        return Player_char.RIGHT
-    elif direction == Player_char.RIGHT:
-        return Player_char.LEFT
-    else:
-        print("YO WHAT THE FUCK GIMME A REAL DIRECTION")
-        return Player_char.RIGHT
+def open_test_file():
+    return open("test.txt", 'wb')
 
 if __name__ == "__main__":
+
+    # initialize curses for non-blocking keyboard input
+    stdscr = curses.initscr()
+    curses.noecho()
+    curses.cbreak()
+    stdscr.keypad(True)
+    stdscr.nodelay(True)
+
     shade_id = 0
-    # position = 0
-    # direction = Player_char.RIGHT
+
     player = Player()
+    key = None
+    paused = False
+    
+    # ### TEST LOOP
+    # with open_test_file() as printer:
+    #     while True:
+    #         time.sleep(0.1)
+    #         player.advance()
+    #         print(player.position)
+    #         printer.write(receipt_printer.get_line(int(player.position), player.get_direction_char(), Block_char.SHADES[shade_id])+b"\n")
 
-    with receipt_printer.open_descriptor() as printer:
+
+    ### REAL GAME LOOP
+    # with receipt_printer.open_descriptor() as printer:
+    with open_test_file() as printer:
         printer.write(receipt_printer.select_code_page(1))
+        
+        while (key != ord('x')):
+            time.sleep(0.1)
 
-        for i in range(30):
-            if not((i+1) % 10):
-                shade_id += 1
-
-            printer.write(receipt_printer.get_line(player.position, player.get_direction_char(), Block_char.SHADES[shade_id])+b"\n")
+            # Handle key input
+            key = stdscr.getch()
+            curses.flushinp()  # flush buffer key buffer to diminish delay
             
-            
-            player.advance()
+            if (key == curses.KEY_RIGHT):
+                player.direction = 0
+            elif (key == curses.KEY_LEFT):
+                player.direction = 1
+            elif (key == ' '):
+                paused = not paused
 
+            if not paused:
+                player.advance()
+                
+            printer.write(receipt_printer.get_line(int(player.position), player.get_direction_char(), Block_char.SHADES[shade_id])+b"\n")
 
         printer.write(receipt_printer.select_code_page(0))
+
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+    curses.endwin()
--- receipt_printer.py
+++ receipt_printer.py
@@ -21,30 +21,65 @@ class Player_char:
 ESC = b"\x1B"  # ESC character
 
 # PAGE_WIDTH = 48
-PAGE_WIDTH = 10
+PAGE_WIDTH = 20
 
-# wrapper to use with a "with" statement
+"""wrapper to use with a "with" statement"""
 def open_descriptor():
     return open("/dev/lp0", 'wb')
 
-# Select code page to use specific character set
-# Notably: 0 is the default, 1 is cp437
-# cp437 has single byte shade blocks
+""" select_code_page(n: int)
+n: code page number (defined by printer)
+
+Select code page to use specific character set
+Notably: 0 is the default, 1 is cp437
+cp437 has single byte shade blocks
+"""
 def select_code_page(n: int):
     return ESC + b"\x1D\x74" + bytes([n])
 
-### get_line(position, direction, shade)
-# position:         int position of player
-# direction_char:   direction character
-# shade:            line shade
-# returns game line with proper formatting
-def get_line(position, direction_char, shade):
-    return Block_char.EMPTY * 20 + (position) * shade + direction_char + (PAGE_WIDTH - position - 1) * shade
 
-### test_shade(printer)
-# prints a gradient to test the characters work as intended.
-#
-# printer: printer file descriptor
+""" get_line(position, direction, shade)
+position:         int position of player
+direction_char:   direction character
+shade:            line shade
+
+returns game line with proper formatting
+NOTE: DEPRECATED
+"""
+def get_line(position, direction_char, shade):
+    return Block_char.EMPTY * 15 + (position) * shade + direction_char + (PAGE_WIDTH - position - 1) * shade
+
+""" str_to_print(string)
+string: string to be formatted for printing
+        eg. "     LLL\L   FFFFF    "
+
+Converts a simple representation of the line to be printed.
+
+' ' -> empty block
+'L' -> light shade block
+'M' -> medium shade block
+'H' -> heavy shade block
+'F' -> ful; shade block
+"""
+def str_to_print(string):
+    converter = {
+        ' ' : Block_char.EMPTY,
+        'L' : Block_char.LIGHT,
+        'M' : Block_char.MED,
+        'H' : Block_char.HEAVY,
+        'F' : Block_char.FULL,
+        '\\': Player_char.RIGHT,
+        '/' : Player_char.LEFT
+    }
+
+    result = [converter[char] for char in string]
+    return ''.join(result)
+
+""" test_shade(printer)
+printer: printer file descriptor
+
+prints a gradient to test the characters work as intended.
+"""
 def test_shade(printer):
     for s in Block_char.SHADES:
         for _ in range(5):
